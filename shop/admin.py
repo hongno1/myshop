0a306a09ce1fba202857e64cb82e2b8801f5c7ed
hongno1 <675551911@qq.com>
2018-04-26 22:29:14 +0800

    购物商店

---
+++ shop/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Category, Product
+
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'stock', 'available', 'created', 'updated')
+    list_editable = ('price', 'stock', 'available')
+    list_filter = ('available', 'price', 'created')
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
+
+
+
+
+
